c6bc18dc17d439d07782603c19e61f5b47123a21
tommyboiiii004 <thomastharun004@gmail.com>
2023-06-27 09:02:31 +0530

    added action to edit button

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader, } from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
-import EditContact from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 
 
 const router = createBrowserRouter([
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
