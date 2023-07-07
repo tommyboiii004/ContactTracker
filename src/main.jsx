acc18574dbe6f207fefcfe00bd71101f2d7b28d1
tommyboiiii004 <thomastharun004@gmail.com>
2023-07-07 11:37:53 +0530

    Created the delete action

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader, } from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import EditContact, { action as editAction } from "./routes/edit";
-
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +29,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
