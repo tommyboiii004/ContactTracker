a343a1e2926053959d76c915e572a17ae0deb356
tommyboiiii004 <thomastharun004@gmail.com>
2023-07-08 11:26:42 +0530

    Finished mutation without navigation, added star

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom'
 import './index.css'
 import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader, } from "./routes/contact";
+import Contact, { loader as contactLoader, action as contactAction, } from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction
       },
       {
         path: "contacts/:contactId/edit",
